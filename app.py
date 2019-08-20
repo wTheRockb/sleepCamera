b86a8b62563f59a0da897dac92896e5f19ac35c7
Woody Butler <forrestwbutler@gmail.com>
2019-08-19 20:47:27 -0400

    Add initial delay and interpreter metadata

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
+#!/usr/bin/env python
 from picamera import PiCamera
 from time import sleep
 import datetime
 import time
 
-
+INITIAL_DELAY_HOURS = 1
 MAX_HOURS = 10
 STILLS_PER_HOUR = 12
 
@@ -13,9 +14,10 @@ def record_still():
         camera.start_preview()
         time.sleep(2)
         camera.capture('stills/' + current_time + '.jpg')
-    
+
 if __name__ == "__main__":
     i = 0
+    sleep(3600 * INITIAL_DELAY_HOURS)
     max_stills = MAX_HOURS * STILLS_PER_HOUR
     while(i < max_stills):
         record_still()
